53d6b5db8af8757ebf389c77ec8ba6dabfb9b6ba
Majdi Boulabiar <Boulabiar.mejdi@gmail.com>
2020-11-09 20:48:13 +0100

    -- layout 1 login --

---
+++ app/src/main/java/com/example/fastdoctor/signUpClient.java
@@ -0,0 +1,16 @@
+package com.example.fastdoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import static com.example.fastdoctor.R.*;
+
+public class signUpClient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_sign_up_client);
+    }
+}
